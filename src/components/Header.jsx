9e99cb4ee3e7b400054eb78963af4fbe5288b754
Andrew Cao <andrewcao618@gmail.com>
2025-03-15 17:35:03 -0400

    Created Header and Added Router Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import bagIcon from "../assets/shopping-bag.svg";
+
+function Header() {
+  return (
+    <nav className="fixed flex w-screen items-center justify-between bg-[#FEF9F3] p-8 font-roboto font-light">
+      <div className="flex flex-1">
+        <ul className="flex gap-4">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="Shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="Shop/Mens">Mens</Link>
+          </li>
+          <li>
+            <Link to="Shop/Womens">Womens</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="flex items-center justify-center">
+        <h1 className="font-ledger text-xl italic md:text-2xl lg:text-4xl">EMBERS</h1>
+      </div>
+      <div className="flex flex-1 justify-end">
+        <Link to="cart">
+          <img className="w-7" src={bagIcon} alt="cart"></img>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/components/Womens.jsx
@@ -0,0 +1,9 @@
+function Womens() {
+  return (
+    <div>
+      <h1>Womens</h1>
+    </div>
+  );
+}
+
+export default Womens;
