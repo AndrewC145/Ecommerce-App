ef30ed14878302c80dba21d2da0a5eac8a432e4a
Andrew Cao <andrewcao618@gmail.com>
2025-03-16 16:20:17 -0400

    Changed Hero text and made font bolder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ function Hero() {
     <main>
       <div className="h-screen bg-[url(./assets/images/hero-bg.jpg)] bg-cover bg-center bg-no-repeat">
         <div className="flex h-full flex-col items-center justify-center gap-4 text-center text-white md:gap-8 lg:gap-12">
-          <h1 className="font-roboto text-xl md:text-3xl lg:text-6xl xl:text-7xl">
-            You Deserve A New Look
+          <h1 className="font-roboto text-xl font-extrabold md:text-3xl lg:text-6xl xl:text-7xl">
+            TRENDY STREETWEAR, <br /> MADE FOR YOU
           </h1>
           <Link
             className="w-40 rounded-md bg-[#FEF9F3] p-2 font-medium text-black transition duration-300 hover:bg-[#ccc8c3]"
