a47040107dc4687d47690390d5008b4f0b0ab45e
Andrew Cao <andrewcao618@gmail.com>
2025-03-17 17:24:19 -0400

    Added Deals Section

---
+++ src/components/Home/Deal.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function Deal() {
+  return (
+    <div className="px-40 py-32">
+      <div className="relative h-[60vh] bg-[url(./assets/images/streetwear-deals.jpg)] bg-cover bg-no-repeat">
+        <div className="absolute bottom-[20%] left-10 flex w-1/4 flex-col items-start gap-2 text-white">
+          <h3 className="text-4xl font-bold">Special Deals</h3>
+          <p className="text-lg">
+            Discover a fresh new trend of streetwear with exclusive deals from Embers. Give new
+            pieces a home to express yourself. Find uniqueness like no one else. Explore now!
+          </p>
+          <Link className="font-medium" to="Shop">
+            Visit Shop
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Deal;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,20 @@
 import Hero from "./Hero";
 import About from "./About";
 import HeroClothing from "./HeroClothing";
+import Deal from "./Deal";
 import womensStreetwear from "../../assets/images/womens-streetwear.jpg";
 import mensStreetwear from "../../assets/images/mens-streetwear.jpg";
 
 function Home() {
   return (
-    <main>
+    <main className="bg-[#FEF9F3]">
       <Hero></Hero>
       <About></About>
       <div className="flex flex-col gap-2 md:flex-row md:gap-3 lg:gap-4">
         <HeroClothing category="Mens" image={mensStreetwear}></HeroClothing>
         <HeroClothing category="Womens" image={womensStreetwear}></HeroClothing>
       </div>
+      <Deal></Deal>
     </main>
   );
 }
