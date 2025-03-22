6c53ae90a527950bce2bd333a30f859507810dac
Andrew Cao <andrewcao618@gmail.com>
2025-03-22 10:52:45 -0400

    Added Home Products

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 function ProductCard({ image, title, price }) {
   return (
-    <div>
-      <img src={image} alt={title} />
+    <div className="flex flex-col items-center gap-2 font-roboto">
+      <img className="" src={image} alt={title} />
       <h5>{title}</h5>
       <p>{price}</p>
     </div>
