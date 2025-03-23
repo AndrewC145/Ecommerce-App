cc71c1238e416c3f7f9be654979df77209ea9555
Andrew Cao <andrewcao618@gmail.com>
2025-03-23 14:31:44 -0400

    Fixed Footer Placement and Text/Image sizing

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -8,8 +8,6 @@ function ProductPage() {
 
   const product = products ? products.find((product) => product.id === idName) : null;
 
-  console.log(product);
-
   if (error) {
     console.error(error);
     return (
@@ -28,14 +26,20 @@ function ProductPage() {
   }
 
   return (
-    <div>
-      <div>
-        <img src={product.image} alt={product.title} />
-        <div>
-          <div>
-            <h2>{product.title}</h2>
-            <p>{product.price}</p>
-            <p>{product.description}</p>
+    <div className="h-screen px-4 md:px-8 lg:px-10 xl:px-12">
+      <div className="my-6 flex flex-col items-center justify-center gap-4 sm:items-start md:my-9 md:flex-row md:gap-8 lg:my-12 lg:gap-12 xl:my-20 xl:gap-16">
+        <img
+          className="size-[50%] sm:size-[40%] md:size-[35%] lg:size-[30%] xl:size-[25%]"
+          src={product.image}
+          alt={product.title}
+        />
+        <div className="flex w-[60%] flex-col gap-4">
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold md:text-3xl lg:text-4xl xl:text-5xl">
+              {product.title}
+            </h2>
+            <p className="text-base font-semibold sm:text-lg md:text-xl">${product.price}</p>
+            <p className="text-sm sm:text-base md:text-lg">{product.description}</p>
           </div>
         </div>
       </div>
